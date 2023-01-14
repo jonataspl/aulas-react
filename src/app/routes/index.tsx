21db3393a0df11422b4da34fdd79539a4bb7b631
Jonatas Lima <110483641+jonataspl@users.noreply.github.com>
2023-01-14 19:42:40 -0300

    Add dashboard
    Add dashboard page and react router dom

---
+++ src/app/routes/index.tsx
@@ -0,0 +1,18 @@
+import {
+  Route,
+  Routes as Switch,
+  BrowserRouter,
+  Navigate,
+} from "react-router-dom";
+import { Dashboard } from "../pages";
+
+export const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/pagina-inicial" element={<Dashboard />} />
+        <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
